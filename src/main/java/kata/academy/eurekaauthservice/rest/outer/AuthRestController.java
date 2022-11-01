0283a4302bc1c3d73e375c7f318e354f9b2116a1
OlyaBystrova <103462368+OlyaBystrova@users.noreply.github.com>
2022-11-01 18:40:41 +0300

    First Commit (#7)

--- src/main/java/kata/academy/eurekaauthservice/rest/outer/AuthRestController.java
+++ src/main/java/kata/academy/eurekaauthservice/rest/outer/AuthRestController.java
@@ -1,5 +1,9 @@
 package kata.academy.eurekaauthservice.rest.outer;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kata.academy.eurekaauthservice.api.Data;
 import kata.academy.eurekaauthservice.model.dto.UserDto;
 import kata.academy.eurekaauthservice.service.UserService;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@Tag(name = "AuthRestController", description = "Регистрация и аутентификация пользователя")
 @RequiredArgsConstructor
 @Validated
 @RestController
@@ -21,12 +26,20 @@ public class AuthRestController {
 
     private final UserService userService;
 
+    @ApiOperation(value = "register", notes = "Регистрация пользователя")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Успешная регистрация пользователя"),
+            @ApiResponse(code = 400, message = "Ошибка при регистрации пользователя, проверьте корректность полей ввода")})
     @PostMapping("/registration")
     public ResponseEntity<Void> register(@RequestBody @Valid UserDto dto) {
         userService.register(dto);
         return ResponseEntity.ok().build();
     }
 
+    @ApiOperation(value = "authenticate", notes = "Аутентификация пользователя")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Успешная аутентификация пользователя"),
+            @ApiResponse(code = 400, message = "Ошибка при аутентификации пользователя, проверьте корректность полей ввода")})
     @PostMapping("/login")
     public ResponseEntity<Data<String>> authenticate(@RequestBody @Valid UserDto dto) {
         return ResponseEntity.ok(Data.of(userService.authenticate(dto)));
